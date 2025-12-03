1a4f75fd6e3958416cf92d8393c4288e54e1d5ea
ALUNO <beatriz.v.alves6@aluno.senai.br>
2025-12-03 16:12:33 -0300

    exercicio02

--- app.js
+++ app.js
@@ -1,40 +1,48 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const PORT = 8081;
 
 app.use(express.json());
 
-//Criar rota 
-app.post("/usuario/novo", (req, res) =>{
+//Rota /soma
+app.post('/soma', (req, res) => {
     try {
-        //Criar as informções que será necessaria para criar um novo usuário
-        const {usuario :{nome, email, senha}} = req.body;
+        //
+        const { valores } = req.body;
+    
+        //O comando "!Array.isArray(valores)" verificara se valores será uma array, se for retorna com true, caso contrário retorna como false 
+        // Verifica se o campo existe e se é um array
+        if (!Array.isArray(valores)) {
+            return res.status(400).json("Campos obrigatótios não respondidos corretamnentes!");
+        }
+    
+        // Verifica se todos os valores são numéricos
+        //".filter" verifica se cada item é uma array 
+        //"valores => typeof valores !== number" verifica o item é um número 
+        //"isNaN(valores)" verificara se valores é um número válido
+        const invalidos = valores.filter(valores => typeof valores !== "number" || isNaN(valores));
+    
+        //O comando "invalidos.length > 0 " verifica se a array invalidos nao esta vazio, caso esteja retornará uma mensagem no json dizendo que as informações colocadas pelo usuários é inválida
+        if (invalidos.length > 0) {
+            return res.status(400).json("Valores inválidos. Verifique os itens colocados são válidos!");
+        }
+    
+        //"valores.reduce" pega a array e o tranforma em apenas um unico valor 
+        // Realiza o cálculo utilizando reduce
+        let resultado = 0;
+        for (let i = 0; i < valores.length; i++) {
+            resultado += valores[i];
+        }
 
-        //"!" = "nome == undefined"
-        //Verificar se o Nome será valido 
-        if (! nome || nome.length < 3) {
-            return res.status(400).json("Erro: Nome inválido! Necessário mais de 3 carateres!");
-        }//"nome.length > 3" Esse comando verificará se a Variável "Nome" será maior que 3 caracteres;
-        //".lenght" comando que retorna a quantidade de caracteres cadastrado dentro da string;
-
-        //Verificar se email é valido
-        if (! email || !email.includes("@")){
-            return res.status(400).json("Erro: Email não inválido!");
-        }//"email.includes(@)" Comando que verificará se "Email" terá "@"
-        //".includes(@)" Comando que verificará que na string ou na array contém "@"
-
-        if (! senha || rssenha.length < 8) {
-            return res.status(400).json(`Erro: Senha inválida! Necessário ter menos de 8 caracteres! `);
-        }//"senha.length < 8 " Verificará se "Senha" vao ser maior que 8 caracteres  
-
-        res.status(200).json("Usuário cadastrado com sucesso!");
+        //Se tudo ocorrer corretamente aparecerá para o usuário o resultado do cálculo em formado de JSON
+        return res.status(200).json(`Resultado:${resultado}`);  
 
     } catch (error) {
         console.log("Erro:", error);
         res.status(500).json({errorMessage: error});
     }
-});
+});  
 
-app.listen(PORT, () =>{
+app.listen(PORT, () => {
     console.log(`Servidor rodando em localhost:${PORT}`);
 });
